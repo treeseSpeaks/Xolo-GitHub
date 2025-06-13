715aeb5efa0d9c146d52d1f0fa33565a2e82dbc7
treeseSpeaks <koenigmonkey@gmail.com>
2025-06-13 13:47:29 -0400

    Initcommit agian

---
+++ README.md
@@ -0,0 +1,3 @@
+# XoloPerforce
+
+Developed with Unreal Engine 5
---
+++ Source/XoloPerforce/XoloPerforce.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "XoloPerforce.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, XoloPerforce, "XoloPerforce" );
